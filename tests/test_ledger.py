61acb5d65206653b0567d1056aea1c8689ebf1c7
Md Shahedur Rahman <mr6139@nyu.edu>
2026-01-11 23:39:44 -0500

    Add create and list test

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -1,2 +1,11 @@
 import pytest
 from budgetbuddy.core.ledger import Ledger
+
+
+def test_create_and_list_sorted():
+    led = Ledger()
+    led.create("2026-01-02", "expense", "Food", 10, "Lunch")
+    led.create("2026-01-01", "income", "Salary", 1000, "Paycheck")
+    txs = led.list_all()
+    assert len(txs) == 2
+    assert txs[0].tx_date == "2026-01-01"
